c06df6724cb154547622ef67ab2e57674173aa74
JPetsis <snowblind777@gmail.com>
2020-01-04 17:11:29 +0100

    corrected tests & added GET and POST functionality

--- test_bucketlist.py
+++ test_bucketlist.py
@@ -5,58 +5,64 @@ import json
 from app import create_app, db
 
 
-
 class BucketlistTestCase(unittest.TestCase):
-
   """This class represents the bucketlist test case"""
 
   def setUp(self):
     """Define test variables and initialize app."""
-    self.app = create_app(config_name = 'testing')
+    self.app = create_app(config_name="testing")
     self.client = self.app.test_client
-    self.bucketlist = {'name': 'Go to Bora Bora for vacation'}
+    self.bucketlist = {'name': 'Go to Borabora for vacation'}
 
     # binds the app to the current context
     with self.app.app_context():
       # create all tables
       db.create_all()
 
-
   def test_bucketlist_creation(self):
     """Test API can create a bucketlist (POST request)"""
-    res = self.client().post('/bucketlists', data = self.bucketlist)
+    res = self.client().post('/bucketlists/', data=self.bucketlist)
     self.assertEqual(res.status_code, 201)
-    self.assertIn('Go to Bora Bora', str(res.data))
+    self.assertIn('Go to Borabora', str(res.data))
 
   def test_api_can_get_all_bucketlists(self):
     """Test API can get a bucketlist (GET request)."""
-    res = self.client().post('/bucketlists', data = self.bucketlist)
+    res = self.client().post('/bucketlists/', data=self.bucketlist)
     self.assertEqual(res.status_code, 201)
-    res = self.client().post('/bucketlists')
+    res = self.client().get('/bucketlists/')
     self.assertEqual(res.status_code, 200)
-    self.assertIn('Go to Bora Bora', str(res.data))
+    self.assertIn('Go to Borabora', str(res.data))
 
   def test_api_can_get_bucketlist_by_id(self):
-    """Test API can edit an existing bucketlist. (PUT request)"""
-    rv = self.client().post('/bucketlists', data = self.bucketlist)
+    """Test API can get a single bucketlist by using it's id."""
+    rv = self.client().post('/bucketlists/', data=self.bucketlist)
     self.assertEqual(rv.status_code, 201)
     result_in_json = json.loads(rv.data.decode('utf-8').replace("'", "\""))
-    result = self.client().get('/bucketlists/{}'.format(result_in_json['id']))
+    result = self.client().get(
+        '/bucketlists/{}'.format(result_in_json['id']))
     self.assertEqual(result.status_code, 200)
-    self.assertIn('Go to Bora Bora', str(result.data))
+    self.assertIn('Go to Borabora', str(result.data))
 
   def test_bucketlist_can_be_edited(self):
     """Test API can edit an existing bucketlist. (PUT request)"""
-    rv = self.client().post('/bucketlists', data = {'name': 'Eat, pray and love'})
+    rv = self.client().post(
+        '/bucketlists/',
+        data={'name': 'Eat, pray and love'})
     self.assertEqual(rv.status_code, 201)
-    rv = self.client().put('/bucketlists/1', data = {"name": "Dont just eat, but also pray and love :-)"})
+    rv = self.client().put(
+        '/bucketlists/1',
+        data={
+            "name": "Dont just eat, but also pray and love :-)"
+        })
     self.assertEqual(rv.status_code, 200)
     results = self.client().get('/bucketlists/1')
     self.assertIn('Dont just eat', str(results.data))
 
   def test_bucketlist_deletion(self):
     """Test API can delete an existing bucketlist. (DELETE request)."""
-    rv = self.client().post('/bucketlists/', data = {'name': 'Eat, pray and love'})
+    rv = self.client().post(
+        '/bucketlists/',
+        data={'name': 'Eat, pray and love'})
     self.assertEqual(rv.status_code, 201)
     res = self.client().delete('/bucketlists/1')
     self.assertEqual(res.status_code, 200)
@@ -74,4 +80,4 @@ class BucketlistTestCase(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
